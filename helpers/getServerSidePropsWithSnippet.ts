bf1ca07de4b001a3f898d5e9d7ff81d49f8e0a0f
Jan Vorcak <vorcak@webscope.io>
2022-04-22 22:39:06 +0200

    Move processing to a server

--- helpers/getServerSidePropsWithSnippet.ts
+++ helpers/getServerSidePropsWithSnippet.ts
@@ -1,5 +1,10 @@
 import { GetServerSideProps } from 'next'
 import { supabaseClient } from '@supabase/supabase-auth-helpers/nextjs'
+import {unified} from "unified";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeFormat from 'rehype-format'
+import rehypeStringify from 'rehype-stringify'
 
 export const getServerSidePropsWithSnippet: GetServerSideProps = async (
   context
@@ -11,6 +16,17 @@ export const getServerSidePropsWithSnippet: GetServerSideProps = async (
     .select()
     .eq('id', id)
     .single()
+
+  // snippet.content
+  const text = await unified()
+      .use(remarkParse)
+      .use(remarkRehype)
+      .use(rehypeFormat)
+      .use(rehypeStringify)
+      .process(snippet.content)
+
+  // console.log({text})
+  snippet.content = text.value
 
   if (snippet === null) {
     return {
--- pages/[id]/[slug].tsx
+++ pages/[id]/[slug].tsx
@@ -1,23 +1,19 @@
-import React, { FC, ReactElement } from 'react'
+import React, { ReactElement } from 'react'
 import Layout from '../../components/Layout'
 import { GetServerSideProps } from 'next'
-import ReactMarkdown from 'react-markdown'
 import { NextPageWithLayout } from '../_app'
 import { Snippet } from '../../types'
-import remarkGfm from 'remark-gfm'
-import rehypeHighlight from 'rehype-highlight'
 import { useUser } from '@supabase/supabase-auth-helpers/react'
 import { getServerSidePropsWithSnippet } from '../../helpers/getServerSidePropsWithSnippet'
 import { PencilAltIcon } from '@heroicons/react/solid'
 import Button from '../../components/Button'
 
+
 const Slug: NextPageWithLayout<{ snippet: Snippet }> = ({ snippet }) => {
   const { user } = useUser()
   return (
     <div className="prose">
-      <ReactMarkdown remarkPlugins={[remarkGfm, rehypeHighlight]}>
-        {snippet.content as string}
-      </ReactMarkdown>
+      <div dangerouslySetInnerHTML={{ __html: snippet.content as string }}/>
       {user && snippet && user.id === snippet.author && (
         <Button
           as="a"
