abbadf2423018ea75292b4aaa7e54fc58471ce92
Jan Vorcak <vorcak@webscope.io>
2022-05-04 07:54:53 +0200

    try

--- pages/api/image/[id].ts
+++ pages/api/image/[id].ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { supabaseClient } from '@supabase/supabase-auth-helpers/nextjs'
-import chromium from 'chrome-aws-lambda'
+const playwright = require('playwright-aws-lambda')
 
 type Data = {
   name: string
@@ -24,23 +24,24 @@ export default async function handler(
 
   const { download } = req.query
 
-  const browser = await chromium.puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath,
-    headless: true,
-    ignoreHTTPSErrors: true,
-  })
+  // const browser = await chromium.puppeteer.launch({
+  //   args: chromium.args,
+  //   defaultViewport: chromium.defaultViewport,
+  //   executablePath: await chromium.executablePath,
+  //   headless: true,
+  //   ignoreHTTPSErrors: true,
+  // })
+  const browser = await playwright.launchChromium()
+
   const page = await browser.newPage()
-  page.setUserAgent(
-    'Opera/9.80 (J2ME/MIDP; Opera Mini/5.1.21214/28.2725; U; ru) Presto/2.8.119 Version/11.10'
-  )
+  // page.setUserAgent(
+  //   'Opera/9.80 (J2ME/MIDP; Opera Mini/5.1.21214/28.2725; U; ru) Presto/2.8.119 Version/11.10'
+  // )
 
   await page.goto(
-    encodeURI(`${getURL()}/44/how-to-kill-a-process-in-unix-system`),
-    { waitUntil: 'networkidle0' }
+    encodeURI(`${getURL()}/44/how-to-kill-a-process-in-unix-system`)
   )
-  await page.setViewport({ width: 1200, height: 675, deviceScaleFactor: 2 })
+  await page.setViewportSize({ width: 1200, height: 675 })
 
   const imageBuffer = await page.screenshot()
 
