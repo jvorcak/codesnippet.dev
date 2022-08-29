6a6b47fccc89015982aaf77e4380a08b2bb938a8
Jan Vorcak <vorcak@webscope.io>
2022-08-29 10:25:27 +0200

    Konva

--- components/Kanva.tsx
+++ components/Kanva.tsx
@@ -1,14 +1,29 @@
-import React from 'react'
-import { Stage, Layer, Text } from 'react-konva'
+import React, { useEffect } from 'react'
+import Konva from 'konva'
 
 const Kanva = () => {
-  return (
-    <Stage width={500} height={500}>
-      <Layer>
-        <Text text="Try click on rect" />
-      </Layer>
-    </Stage>
-  )
+  useEffect(() => {
+    const stage = new Konva.Stage({
+      container: 'container',
+      width: 1200,
+      height: 675,
+    })
+
+    const layer = new Konva.Layer()
+    stage.add(layer)
+
+    const text = new Konva.Text({
+      x: stage.width() / 2,
+      y: 15,
+      text: 'Simple Text',
+      fontSize: 30,
+      fontFamily: 'Calibri',
+      fill: 'green',
+      draggable: true,
+    })
+    layer.add(text)
+  }, [])
+  return <div id="container"></div>
 }
 
 export default Kanva
